d819162cd05134909cb2c04e7c4dd0b7d15fb2c9
Surajit Das <smartsurajit2008@gmail.com>
2019-03-08 16:59:58 +0300

    chat demo

--- chatserver.js
+++ chatserver.js
@@ -10,7 +10,7 @@ app.use(express.static('public'))
 
 
 //routes
-app.get('/', (req, res) => {
+app.get('/', function(req, res){
     res.render('index')
 })
 
@@ -24,26 +24,27 @@ const io = require("socket.io")(server)
 
 
 //listen on every connection
-io.on('connection', (socket) => {
+io.on('connection', function(socket) {
     console.log('New user connected')
 
     //default username
     socket.username = "Anonymous"
 
     //listen on change_username
-    socket.on('change_username', (data) => {
+    socket.on('change_username',function (data) {
         socket.username = data.username
+
     })
 
     //listen on new_message
-    socket.on('new_message', (data) => {
+    socket.on('new_message', function(data){
         //broadcast the new message
         console.log('message',data)
         io.sockets.emit('new_message', {message : data.message, username : socket.username});
     })
 
     //listen on typing
-    socket.on('typing', (data) => {
+    socket.on('typing', function(data){
         socket.broadcast.emit('typing', {username : socket.username})
     })
 })
