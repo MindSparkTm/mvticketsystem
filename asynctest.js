111166298c85e711d193f6223249c714f3122d59
Surajit Das <smartsurajit2008@gmail.com>
2019-03-08 16:11:54 +0300

    chat demo

--- asynctest.js
+++ asynctest.js
@@ -1,10 +1,76 @@
 
 var fs = require('fs');
+var async = require('async');
 
 /* non blocking code */
 
-fs.readFile('hello.txt', 'utf8', function(err, contents) {
-    console.log(contents);
-});
+function sleep(ms){
+    return new Promise(resolve=>{
+        setTimeout(resolve,ms)
+    })
+}
 
-console.log('after calling readFile');
+function foo() {
+    fs.readFile('hello.txt', 'utf8', function (err, contents) {
+
+        if (err) {
+            return err
+
+        } else {
+            return contents
+        }
+    })
+}
+function fruitloop(){
+
+    return new Promise(function (resolve,reject){
+        fs.readFile('hello.txt', 'utf8', function(err, contents) {
+
+            if (err){
+                reject(err)
+            }
+
+            else{
+                resolve(contents)
+            }
+        });
+
+
+    })
+
+}
+
+
+
+
+async function nfruitloop(callback) {
+
+   response= await foo()
+
+    console.log('done')
+
+
+
+
+
+}
+
+
+for(var i =0;i<5;i++) {
+
+
+
+    nfruitloop()
+        .then((message) => {
+            console.log(message);
+        }).catch((error) => {
+        console.log(error.message);
+    });
+
+    fruitloop().then(function (result) {
+
+        console.log('s', result)
+
+    })
+
+}
